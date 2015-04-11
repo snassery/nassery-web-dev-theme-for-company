9915054ffafa9df194ba0c0df0afa4972a6ffcf6
Scott <snassery@me.com>
2015-04-11 11:20:40 -0400

    git pag
    git page update

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -246,7 +246,7 @@
     <div id="copyright">
       <p class="copyright">
         <?php print t('Copyright'); ?> &copy; <?php echo date("Y"); ?>, <?php print $site_name; ?> </p> 
-      <p class="credits"> <?php print t('Designed by'); ?>  <a href="http://www.zymphonies.com">Zymphonies</a></p>
+      <p class="credits"> <?php print t('Designed by'); ?>  <a href="http://www.dev.nasserywebdev.pantheon.com">Nassery Web Development</a></p>
       <div class="clear"></div>
     </div>
   </div>
